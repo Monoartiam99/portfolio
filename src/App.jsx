f5da9304591d99bfec43f33312625fe19dd4809a
Monojit Nandy <nandymonojitcourse99.24@gmail.com>
2026-06-21 22:44:55 +0530

    Create a new portfolio for me

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">Monojit Nandy</div>
+      <ul className="navbar-links">
+        <li className="c1"><a href="#home">About Me</a></li>
+        <li className="c2"><a href="#poject">Projects</a></li>
+        <li className="c3"><a href="#download">Download</a></li>
+        <li className="c4"><a href="#connect">Connect</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default App
